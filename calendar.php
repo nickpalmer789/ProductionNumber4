6c223fa4e991a597ab030745898b8f08e22615a8
Chris <scarolac@gmail.com>
2017-11-03 20:48:12 -0600

    Moved a button, converted more to bootstrap

--- calendar.php
+++ calendar.php
@@ -13,91 +13,70 @@
       //Include the navbar content
       include('templates/navbar.php');
     ?>
-        <div class="whitespace"> </div>
-
-        <div class="calendarspacing">
+        <div class="container-fluid">
             <h1 align="left">
                 <font size="7">My Calendar</font>
             </h1>
-        </div>
 
-        <div class="row">
-
-            <div class="col-sm-8" align="center">
-
-                <div class="calendarspacing">
-                    <div class="totallyacalendar">
-                        this is a calendar
+            <div class="row">
+                <div class="col-sm-8" align="center">
+                    <div class="calendarspacing">
+                        <div class="totallyacalendar">
+                            <p>this is a <del>calendar</del> box </p>
+                        </div>
                     </div>
                 </div>
-
+                <div class="col-sm-4" align="center">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>My Tasks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Do the project</td>
+                            </tr>
+                            <tr>
+                                <td>Make it not incomplete</td>
+                            </tr>
+                            <tr>
+                                <td>This probably won't be a table at the end</td>
+                            </tr>
+                            <tr>
+                                <td>It just looks nice</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Completed Tasks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>I did the project</td>
+                            </tr>
+                            <tr>
+                                <td>I made it not incomplete</td>
+                            </tr>
+                            <tr>
+                                <td>Firetruck</td>
+                            </tr>
+                            <tr>
+                                <td>Rocks</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-            <div class="col-sm-4" align="center">
-
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>My Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Do the project</td>
-                        </tr>
-                        <tr>
-                            <td>Make it not incomplete</td>
-                        </tr>
-                        <tr>
-                            <td>This probably won't be a table at the end</td>
-                        </tr>
-                        <tr>
-                            <td>It just looks nice</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <div class="whitespace"> </div>
-
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Completed Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>I did the project</td>
-                        </tr>
-                        <tr>
-                            <td>I made it not incomplete</td>
-                        </tr>
-                        <tr>
-                            <td>Firetruck</td>
-                        </tr>
-                        <tr>
-                            <td>Rocks</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-
-
-
-
-
-
-
-
-        <?php
+            <?php
         include('templates/footerCopy.php');
       ?>
-
-
-
-            <?php
+        </div>
+        <?php
       include('templates/footerScripts.php');
     ?>
 </body>
--- groupView.php
+++ groupView.php
@@ -13,95 +13,93 @@
       //Include the navbar content
       include('templates/navbar.php');
     ?>
-        <div class="whitespace"> </div>
-
-        <div class="calendarspacing row">
-            <div class="col-xl-6">
-                <label for="groups"><h1 align="left"> <font size="7">Groupname Calendar</font></h1></label>
-            </div>
-            <div align="right" class="dropdown col-xs-6" id="groups">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Groups
-                  </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Group1</a>
-                    <a class="dropdown-item" href="#">Group2</a>
-                    <a class="dropdown-item" href="#">Group3</a>
+        <div class="container-fluid">
+            <div class="row">
+               
+                <div class="col-xs-6">
+                    <font size="7">Groupname Calendar</font>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-
-            <div class="col-sm-8" align="center">
-
-                <div class="calendarspacing">
-                    <div class="totallyacalendar">
-                        <p>this is a <del>calendar</del> box </p>
+                
+                <div class="col-xs-6" id="groups">
+                    <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Groups
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="index.php">Group1</a>
+                        <a class="dropdown-item" href="calendar.php">Group2</a>
+                        <a class="dropdown-item" href="#">Group3</a>
                     </div>
                 </div>
-
+                
             </div>
+            <div class="row">
+                <div class="col-sm-8">
 
-            <div class="col-sm-4" align="center">
+                    <div class="calendarspacing">
+                        <div class="totallyacalendar" align="center">
+                            <p>this is a <del>calendar</del> box </p>
+                        </div>
+                    </div>
 
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Group Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Do the project</td>
-                        </tr>
-                        <tr>
-                            <td>Make it not incomplete</td>
-                        </tr>
-                        <tr>
-                            <td>Meetings etc</td>
-                        </tr>
-                        <tr>
-                            <td>Group things</td>
-                        </tr>
-                    </tbody>
-                </table>
+                </div>
+                <div class="col-sm-4">
 
-                <div class="whitespace"> </div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Group Tasks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Do the project</td>
+                            </tr>
+                            <tr>
+                                <td>Make it not incomplete</td>
+                            </tr>
+                            <tr>
+                                <td>Meetings etc</td>
+                            </tr>
+                            <tr>
+                                <td>Group things</td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Completed Tasks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>I did the project</td>
-                        </tr>
-                        <tr>
-                            <td>I made it not incomplete</td>
-                        </tr>
-                        <tr>
-                            <td>Firetruck</td>
-                        </tr>
-                        <tr>
-                            <td>Rocks</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                    <br>
 
-        </div>
-
-        <?php
-        include('templates/footerCopy.php');
-      ?>
-
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Completed Tasks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>I did the project</td>
+                            </tr>
+                            <tr>
+                                <td>I made it not incomplete</td>
+                            </tr>
+                            <tr>
+                                <td>Firetruck</td>
+                            </tr>
+                            <tr>
+                                <td>Rocks</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
             <?php
-      include('templates/footerScripts.php');
-    ?>
+                include('templates/footerCopy.php');
+            ?>
+        </div>
+
+        <?php
+            include('templates/footerScripts.php');
+        ?>
 </body>
 
 </html>
