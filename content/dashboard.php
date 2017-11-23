ab33382149b832fdf2e65fa3f97808933700cef4
Chris <scarolac@gmail.com>
2017-11-23 00:12:45 -0700

    accidental tiny calendar

--- content/dashboard.php
+++ content/dashboard.php
@@ -13,7 +13,7 @@
 		    include('../templates/navbar.php');	
             include('../php/session.php');
 	    ?>
-        <div class="container-fluid">
+        <div class="container">
             <h1 class="text-center">
                 <font size="7">Dashboard</font>
             </h1>
@@ -88,14 +88,14 @@
                     <button class="btn btn-block btn-success" id="updateBtn">Update Timer</button>
                 </div>
             </div>
-
-        </div>
-        <?php
-            include('../templates/footerCopy.php');
-        ?>
             <?php
-            include('../templates/footerScripts.php');
-        ?>
+                include('../templates/footerCopy.php');
+            ?>
+        </div>
+        
+            <?php
+                include('../templates/footerScripts.php');
+            ?>
 </body>
 
 </html>
