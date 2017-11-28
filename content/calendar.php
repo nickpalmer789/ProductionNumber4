cd60c624eb81f4a3ef502aee24718aee9dfdf330
Chris Scarola <scarolac@colorado.edu>
2017-11-27 17:33:04 -0700

    move script to bottom of calendar page

--- content/calendar.php
+++ content/calendar.php
@@ -6,6 +6,41 @@
       //Include the header content
       include('../templates/headercontent.php')
     ?>
+
+</head>
+
+<body>
+    <?php
+      //Include the navbar content
+        include('../templates/navbar.php');
+        include('../php/session.php');
+    ?>
+        <div class="container-fluid">
+            <h1 align="left">
+                <font size="7">My Calendar</font>
+            </h1>
+
+            <div class="row">
+                <div class="col-sm-8" align="center">
+                    <div id="calendar">
+                       <!-- Full calendar js uses this spot -->
+                    </div>
+                </div>
+                <div class="col-sm-4" align="center">
+                    <?php
+                        include('../php/taskHandler.php');
+                    ?>
+                </div>
+
+            </div>
+            <?php
+                include('../templates/footerCopy.php');
+            ?>
+        </div>
+        <?php
+            include('../templates/footerScripts.php');
+        ?>
+</body>
     <script>
 
 	$(document).ready(function() {
@@ -81,39 +116,5 @@
 	});
 
 </script>
-</head>
-
-<body>
-    <?php
-      //Include the navbar content
-        include('../templates/navbar.php');
-        include('../php/session.php');
-    ?>
-        <div class="container-fluid">
-            <h1 align="left">
-                <font size="7">My Calendar</font>
-            </h1>
-
-            <div class="row">
-                <div class="col-sm-8" align="center">
-                    <div id="calendar">
-                       <!-- Full calendar js uses this spot -->
-                    </div>
-                </div>
-                <div class="col-sm-4" align="center">
-                    <?php
-                        include('../php/taskHandler.php');
-                    ?>
-                </div>
-
-            </div>
-            <?php
-                include('../templates/footerCopy.php');
-            ?>
-        </div>
-        <?php
-            include('../templates/footerScripts.php');
-        ?>
-</body>
 
 </html>
