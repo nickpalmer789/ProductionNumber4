855793f9dfd470fd35332c003c3daf679b338056
Chris Scarola <scarolac@colorado.edu>
2017-11-27 17:00:31 -0700

    navbar restored to index with logout functioning correctly

--- index.php
+++ index.php
@@ -9,52 +9,10 @@
 </head>
 
 <body>
-    <!-- Navbar but the code -->
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">Planit</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-            </ul>
-
-            <button type="button" class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#loginModal">Log In</button>
-
-            <!-- The login modal (until it gets its own file again, maybe) -->
-            <div class="modal fade" id="loginModal" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <form action="php/loginHandler.php" method="post">
-                                <div class="imgcontainer">
-                                    <img src="assets/icons/planiticon.png" alt="Planit" class="avatar">
-                                    <h2>Planit Login</h2>
-                                </div>
-
-                                <label for="usr"><b>Username</b></label>
-
-                                <input type="text" class="form-control" placeholder="Enter Username" name="username" required>
-
-                                <label for="pwd"><b>Password</b></label>
-
-                                <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
-                                <input type="checkbox" checked="checked"> Remember me
-                                <button class="btn-success btn-lg btn-block" type="submit">Login</button>
-
-
-                                <br>
-                                <button type="button" class="btn-danger btn-lg btn-block" data-dismiss="modal">Cancel</button>
-                                <span class="psw">Forgot <a href="#">password?</a></span>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Modal -->
-
-        </div>
-    </nav>
+   <?php
+      //Include the navbar content
+        include('templates/navbar.php');
+    ?>
 
     <!-- End Navbar -->
     <div class="jumbotron">
--- php/logout.php
+++ php/logout.php
@@ -1,10 +1,8 @@
 <?php
     include('../php/session.php');
 
-    //session_unset();
     $_SESSION = array();
     session_destroy();
-    $_SESSION["logged_in"] = FALSE;
     header("location: /index.php");
 
 ?>
