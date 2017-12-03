ebf77fa8ea7fc9ceb2cf406ee5970b6e11aa9f8d
melissamantey <melissa.mantey@colorado.edu>
2017-12-03 16:01:30 -0700

    create tasks test

--- tests/automated_tests.py
+++ tests/automated_tests.py
@@ -8,8 +8,8 @@ import requests
 class SeleniumCBT(unittest.TestCase):
     def setUp(self):
 
-        self.username = "mmantey9@gmail.com"
-        self.authkey  = "uc3530e35d02ec2c"
+        self.username = "scarolac@colorado.edu"
+        self.authkey  = "ue6d54cff39ce185"
 
         self.api_session = requests.Session()
         self.api_session.auth = (self.username,self.authkey)
@@ -28,68 +28,121 @@ class SeleniumCBT(unittest.TestCase):
 
         self.driver.implicitly_wait(20)
 
-    def test_CBT(self):
-        print("Testing Title of Webiste...")
+#    def test_CBT(self):
+#        print("Testing Title of Webiste...")
+#        self.driver.get('http://73.14.69.109/index.php')
+#        self.assertEqual("Planit", self.driver.title)
+#        print("Planit = " + self.driver.title)
+#        print("Success!")
+#        self.test_result = 'pass'
+#        self.driver.quit()
+#       
+#    def test_login(self):
+#        print("Testing login...")
+#        self.driver.get('http://73.14.69.109/index.php')
+#        self.driver.save_screenshot('screenshot.png')
+#        print("Success! Took screenshot")
+#        try:
+#            login_button = WebDriverWait(self.driver, 10).until(
+#                EC.presence_of_element_located((By.ID, "login_button"))
+#            )
+#            login_button.click()
+#            print("Getting username and password...")
+#            self.driver.find_element_by_name('username').send_keys('mema0341')
+#            self.driver.find_element_by_name('password').send_keys('spring2017')
+#            self.driver.save_screenshot('screenshot1.png')
+#            print("Success! Took screenshot1")
+#            self.driver.find_element_by_id('login_button_1').submit()
+#            print("Checking if we redirect to Dashboard")
+#            self.driver.get("http://73.14.69.109/content/dashboard.php")
+#            print("Success! Took screenshot2")
+#            self.driver.save_screenshot('screenshot2.png')
+#            print("Checking for Settings button")
+#            self.driver.find_element_by_id('settings_button')
+#            print("found settings button...")
+#            self.driver.find_element_by_id('logout_button').submit()
+#            print("found logout button! clicking and checking if we see login button... ")
+#            self.driver.save_screenshot('screenshot3.png')
+#            print("Success! Took screenshot3")
+#            print("logging in john1...")
+#            login_button = WebDriverWait(self.driver, 10).until(
+#                EC.presence_of_element_located((By.ID, "login_button"))
+#            )
+#            login_button.click()
+#            self.driver.find_element_by_name('username').send_keys('john1')
+#            self.driver.find_element_by_name('password').send_keys('password')
+#            self.driver.save_screenshot('screenshot4.png')
+#            self.driver.find_element_by_id('login_button_1').submit()
+#            print("Success! Took screenshot4")
+#            print("Checking if we redirect to Dashboard")
+#            self.driver.get("http://73.14.69.109/content/dashboard.php")
+#            self.driver.save_screenshot('screenshot5.png')
+#            print("Success! Took screenshot5")
+#            print("Checking for Settings button")
+#            self.driver.find_element_by_id('settings_button')
+#            print("found settings button...")
+#            self.driver.find_element_by_id('logout_button').submit()
+#            print("found logout button! clicking ... ")
+#            print("Success! Took screenshot6")
+#            self.driver.save_screenshot('screenshot6.png')
+#            print("Test complete.")
+#        finally:
+#            self.test_result='pass'
+#            self.driver.quit()
+        
+    def test_create_task(self):
+        print("Navigating to tasks...")
         self.driver.get('http://73.14.69.109/index.php')
-        self.assertEqual("Planit", self.driver.title)
-        print("Planit = " + self.driver.title)
-        print("Success!")
-        self.test_result = 'pass'
-        self.driver.quit()
-       
-    def test_login(self):
-        print("Testing login...")
-        self.driver.get('http://73.14.69.109/index.php')
-        self.driver.save_screenshot('screenshot.png')
-        print("Success! Took screenshot")
         try:
             login_button = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "login_button"))
             )
             login_button.click()
-            print("Getting username and password...")
             self.driver.find_element_by_name('username').send_keys('mema0341')
             self.driver.find_element_by_name('password').send_keys('spring2017')
-            self.driver.save_screenshot('screenshot1.png')
-            print("Success! Took screenshot1")
             self.driver.find_element_by_id('login_button_1').submit()
-            print("Checking if we redirect to Dashboard")
             self.driver.get("http://73.14.69.109/content/dashboard.php")
-            print("Success! Took screenshot2")
-            self.driver.save_screenshot('screenshot2.png')
-            print("Checking for Settings button")
-            self.driver.find_element_by_id('settings_button')
-            print("found settings button...")
-            self.driver.find_element_by_id('logout_button').submit()
-            print("found logout button! clicking and checking if we see login button... ")
-            self.driver.save_screenshot('screenshot3.png')
-            print("Success! Took screenshot3")
-            print("logging in john1...")
-            login_button = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID, "login_button"))
-            )
-            login_button.click()
-            self.driver.find_element_by_name('username').send_keys('john1')
-            self.driver.find_element_by_name('password').send_keys('password')
-            self.driver.save_screenshot('screenshot4.png')
-            self.driver.find_element_by_id('login_button_1').submit()
-            print("Success! Took screenshot4")
-            print("Checking if we redirect to Dashboard")
-            self.driver.get("http://73.14.69.109/content/dashboard.php")
-            self.driver.save_screenshot('screenshot5.png')
-            print("Success! Took screenshot5")
-            print("Checking for Settings button")
-            self.driver.find_element_by_id('settings_button')
-            print("found settings button...")
-            self.driver.find_element_by_id('logout_button').submit()
-            print("found logout button! clicking ... ")
-            print("Success! Took screenshot6")
-            self.driver.save_screenshot('screenshot6.png')
+            print("adding task called 'automated test task'...")
+            self.driver.find_element_by_name('new_task_button').click()
+            self.driver.find_element_by_name('taskName').send_keys('automated test task')
+            self.driver.find_element_by_name('description').send_keys('automated description')
+            self.driver.find_element_by_name('deadlineDate').send_keys('12/02/2017')
+            self.driver.find_element_by_name('deadlineTime').send_keys('11:55:00 PM')
+            self.driver.find_element_by_name('eta').send_keys('3 hours')
+            self.driver.save_screenshot('screenshot7.png')
+            self.driver.find_element_by_name('createTask').submit()
+            print("Success! Took screenshot7 and screenshot8.png")
+            self.driver.save_screenshot('screenshot8.png')
+            
             print("Test complete.")
         finally:
             self.test_result='pass'
-            self.driver.quit()
+            self.driver.quit() 
         
+#    def test_create_account(self):
+#        print("Testing Create Account...")
+#        self.driver.get('http://73.14.69.109/index.php')
+#        self.driver.save_screenshot('screenshot.png')
+#        print("Success! Took screenshot")
+#        try:
+#            login_button = WebDriverWait(self.driver, 10).until(
+#                EC.presence_of_element_located((By.ID, "login_button"))
+#            )
+#            login_button.click()
+#            print("Getting username and password...")
+#            self.driver.find_element_by_name('username').send_keys('mema0341')
+#            self.driver.find_element_by_name('password').send_keys('spring2017')
+#            self.driver.save_screenshot('screenshot1.png')
+#            print("Success! Took screenshot1")
+#            self.driver.find_element_by_id('login_button_1').submit()
+#            print("Checking if we redirect to Dashboard")
+#            self.driver.get("http://73.14.69.109/content/dashboard.php")
+#            print("Success! Took screenshot2")
+#            self.driver.save_screenshot('screenshot2.png')
+#            print("Test complete.")
+#        finally:
+#            self.test_result='pass'
+#            self.driver.quit() 
 
 
 if __name__ == '__main__':
