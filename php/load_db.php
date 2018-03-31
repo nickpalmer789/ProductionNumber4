4c7531f2bed65a1160946e15c3b9acfef381fb99
Chris Scarola <scarolac@gmail.com>
2018-03-31 00:19:13 +0000

    change db password

--- php/load_db.php
+++ php/load_db.php
@@ -1,7 +1,7 @@
 <?php
 
     // Obtain a connection object by connecting to the db
-    $connection = mysqli_connect('localhost', 'root', 'password', 'planit');
+    $connection = mysqli_connect('localhost', 'root', 'sql', 'planit');
 
     if(mysqli_connect_errno()){
         echo "<h4>Failed to connect to MySQL:</h4>".mysqli_connect_error();
